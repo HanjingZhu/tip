6f3256b5ab20249974919d33690504ef98032a95
Hanjing Zhu <hzhu7@nd.edu>
2020-07-15 13:00:24 -0500

    Update

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -23,10 +23,10 @@ class ViewController: UIViewController {
     }
     @IBOutlet weak var tipControl: UISegmentedControl!
         
-    @IBAction func onTap(_ sender: Any) {
+    @IBAction func onTap( sender: Any) {
         
     }
-    @IBAction func calculateTip(_ sender: Any) {
+    @IBAction func calculateTip( sender: Any) {
         let bill = Double(billAmountTextField.text!) ?? 0
         let tipPercentages = [0.15, 0.18, 0.2]
         
